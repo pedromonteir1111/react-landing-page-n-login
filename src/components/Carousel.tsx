3cd711778b024c35a4beed905dc3bba316d814d3
pedromonteir1111 <pmfm2004@gmail.com>
2025-02-16 03:16:26 -0300

    transition through images

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -1,20 +1,33 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 
-type CarouselProps = {
-    images:string[]
+interface CarouselProps {
+    images: string[];
 }
 
-//TODO: the actual slider
+export default function Carousel({ images }: CarouselProps) {
+    const [index, setIndex] = useState(0);
 
-export default function Carousel({images}:CarouselProps) {
-    
-    const [index, setCount] = useState(0)
+    function nextImage() {
+        setIndex((prevIndex) => (prevIndex === images.length - 1 ? 0 : prevIndex + 1));
+    }
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setIndex((prevIndex) => (prevIndex === images.length - 1 ? 0 : prevIndex + 1));
+        }, 5000);
+
+        return () => clearInterval(interval);
+    }, []);
 
     return (
-        <>
         <div>
-            <img src={images[index]}/>
+            {images.map((image, i) => (
+                <img key={i} src={image}
+                    className={`absolute w-full h-full object-cover transition-opacity duration-500 ${
+                        i === index ? "opacity-100" : "opacity-0"
+                    }`}
+                />
+            ))}
         </div>
-        </>
-    )
+    );
 }
